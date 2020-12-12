d6c68779793dd55c28e823acb0db3371dadc5859
jorgeluis1985 <jorgeluiscamarcobaldovino@gmail.com>
2020-12-12 11:14:12 -0500

    refactored code with <Jumbo> component

--- tests/acceptance/super-rentals-test.js
+++ tests/acceptance/super-rentals-test.js
@@ -1,43 +1,43 @@
-import { module, test } from 'qunit';
-import { click, visit, currentURL } from '@ember/test-helpers';
-import { setupApplicationTest } from 'ember-qunit';
+import { module, test } from 'qunit'
+import { click, visit, currentURL } from '@ember/test-helpers'
+import { setupApplicationTest } from 'ember-qunit'
 
 module('Acceptance | super rentals', function(hooks) {
-  setupApplicationTest(hooks);
+  setupApplicationTest(hooks)
 
   test('visiting /', async function(assert) {
-    await visit('/');
+    await visit('/')
 
-    assert.equal(currentURL(), '/');
-    assert.dom('h2').hasText('Welcome to Super Rentals!');
+    assert.equal(currentURL(), '/')
+    assert.dom('h2').hasText('Welcome to Super Rentals!')
 
-    assert.dom('.jumbo a.button').hasText('About Us');
-    await click('.jumbo a.button');
+    assert.dom('.jumbo a.button').hasText('About Us')
+    await click('.jumbo a.button')
 
-    assert.equal(currentURL(), '/about');
-  });
+    assert.equal(currentURL(), '/about')
+  })
 
   test('visiting /about', async function(assert) {
-    await visit('/about');
+    await visit('/about')
 
-    assert.equal(currentURL(), '/about');
-    assert.dom('h2').hasText('About Super Rentals');
+    assert.equal(currentURL(), '/about')
+    assert.dom('h2').hasText('About Super Rentals')
 
-    assert.dom('.jumbo a.button').hasText('Contact Us');
-    await click('.jumbo a.button');
+    assert.dom('.jumbo a.button').hasText('Contact Us')
+    await click('.jumbo a.button')
 
-    assert.equal(currentURL(), '/getting-in-touch');
-  });
+    assert.equal(currentURL(), '/getting-in-touch')
+  })
 
   test('visiting /getting-in-touch', async function(assert) {
-    await visit('/getting-in-touch');
+    await visit('/getting-in-touch')
 
-    assert.equal(currentURL(), '/getting-in-touch');
-    assert.dom('h2').hasText('Contact Us');
+    assert.equal(currentURL(), '/getting-in-touch')
+    assert.dom('h2').hasText('Contact Us')
 
-    assert.dom('.jumbo a.button').hasText('About');
-    await click('.jumbo a.button');
+    assert.dom('.jumbo a.button').hasText('About')
+    await click('.jumbo a.button')
 
-    assert.equal(currentURL(), '/about');
-  });
-});
+    assert.equal(currentURL(), '/about')
+  })
+})
